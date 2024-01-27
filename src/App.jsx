2592d917ed52e79863821efc58dd115c3e2ef281
Francisco Zambrano <136658451+Francisco-Zambrano@users.noreply.github.com>
2024-01-27 10:51:31 -0300

    iniciando api key

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1>My App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import './index.css'
+
+import { CssBaseline } from '@mui/material'
+import { SnackbarProvider } from 'notistack';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SnackbarProvider>
+      
+      <CssBaseline />
+      <App />
+      
+    </SnackbarProvider>
+  </React.StrictMode>,
+)
